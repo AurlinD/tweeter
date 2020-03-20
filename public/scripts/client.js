faab962515a24479ead2ab7ef9995861bf15d4d8
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-03-20 12:10:19 -0700

    implemented dynamic tweet for footer portion. Dynamic tweets is complete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -31,6 +31,36 @@ const data = [
   }
 ];
 
+// stackoverflow code that calculates JS time;
+// JS time starts at Jan 1st, 1975
+function timeSince(date) {
+  let seconds = Math.floor((new Date() - date) / 1000);
+
+  let interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + " years";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+}
+let aDay = 24 * 60 * 60 * 1000;
+
 const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
@@ -62,7 +92,11 @@ const createTweetElement = function(tweet) {
   $tweet.append($header);
   $tweet.append($main);
 
-  // ...
+  let date = new Date(tweet.created_at);
+  let timeElapsed = timeSince(date);
+  let $footer = $("<footer>").text(`${timeElapsed} ago`);
+  $tweet.append($footer);
+
   return $tweet;
 };
 
